0456b0301cc72af6ffde4bfa5d6908bd9d3cb159
Akash Manohar J <akash@akash.im>
2018-04-11 16:11:33 +0530

    Got working graphql server

--- hello-graphql-py/app.py
+++ hello-graphql-py/app.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from flask import Flask, request
+import graphene
+from pprint import pprint
+import json
 
+class Query(graphene.ObjectType):
+    hello = graphene.String(description='A typical hello world')
 
+    def resolve_hello(self, info):
+        return 'World'
 
+schema = graphene.Schema(query=Query)
 app = Flask(__name__)
+
+
+def format_location(location):
+    return {'line': location.line, 'column': location.column}
+
+def format_error(error):
+    locations = map(error.locations, format_location)
+    return {
+      'message': error.message,
+      'locations': locations
+    }
+
+
+def format_response(graphql_result):
+    response = {'data': graphql_result.data}
+    if graphql_result.invalid:
+        response['errors'] = map(graphql_result.errors, format_error)
+    return json.dumps(response)
+
 
 @app.route("/")
 def hello():
     return "Hello World"
+
+
+@app.route("/graphql", methods=['POST'])
+def graphql():
+    pprint(request.json)
+    result = schema.execute(request.json['query'])
+    return format_response(result)
 
