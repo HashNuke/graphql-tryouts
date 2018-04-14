3877cdcf889ebe899ced4e7903f32c677fa129b7
Akash Manohar J <akash@akash.im>
2018-04-14 21:54:14 +0530

    Complete ID resolvers

--- hello-graphql-rails/app/graphql/hello_graphql_schema.rb
+++ hello-graphql-rails/app/graphql/hello_graphql_schema.rb
@@ -3,21 +3,12 @@ HelloGraphqlSchema = GraphQL::Schema.define do
   query(Types::QueryType)
 
   id_from_object ->(object, type_definition, query_ctx) {
-    node_id = GraphQL::Schema::UniqueWithinType.encode(type_definition.name, object.id)
-    puts("object_from_id: OUTPUT: #{node_id}")
-    return node_id
+    GraphQL::Schema::UniqueWithinType.encode(type_definition.name, object.id)
   }
 
   object_from_id ->(id, query_ctx) {
-    puts("object_from_id: OUTPUT: #{id}")
-    puts(id)
     type_name, item_id = GraphQL::Schema::UniqueWithinType.decode(id)
-
-    puts type_name
-
-    # Now, based on `type_name` and `id`
-    # find an object in your application
-    # ....
+    type_name.constantize.find(item_id)
   }
 
   resolve_type ->(type, obj, ctx) {
