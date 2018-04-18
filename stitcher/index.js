7ce8ed9a11a73b43f13eb5d3e6fcc4a99d111697
Akash Manohar J <akash@akash.im>
2018-04-18 19:08:13 +0530

    Schema stitching for fields

--- stitcher/index.js
+++ stitcher/index.js
@@ -16,6 +16,12 @@ const schemaUrls = [
   "http://localhost:5000/graphql"
 ]
 
+const linkDefs = `
+  extend type User {
+    posts: Post
+  }
+`;
+
 const fetchSchema = async (url) => {
   const link = new HttpLink({ uri: url, fetch });
   const schema = await introspectSchema(link);
@@ -26,14 +32,47 @@ const fetchSchema = async (url) => {
   });
 }
 
+const resolverForPostsByUserId = (mergeInfo) => {
+  (parent, args, context, info) => {
+    const userId = parent.id;
+
+    console.log("RESOLVER FOR POSTS");
+
+    return mergeInfo.delegate(
+      'query',
+      'postsByUserId',
+      {
+        userId,
+      },
+      context,
+      info,
+    );
+  }
+}
+
+let crossResolvers = (mergeInfo) => ({
+  User: {
+    posts: {
+      fragment: "fragment UserFragment on User { id }",
+      resolve: resolverForPostsByUserId(mergeInfo)
+    }
+  }
+})
+
 let schemaFetchPromises = schemaUrls.map(fetchSchema);
 Promise.all(schemaFetchPromises).then((schemas) => {
+  schemas.push(linkDefs);
   const mergedSchema = mergeSchemas({
     schemas: schemas,
+    resolvers: crossResolvers
   });
 
   const app = express();
-  app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: mergedSchema }));
+  app.use(
+    '/graphql',
+    bodyParser.json(),
+    graphqlExpress({ schema: mergedSchema })
+  );
   app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
   app.listen(4000, () => {
     console.log('Go to http://localhost:4000/graphiql to run queries!');
