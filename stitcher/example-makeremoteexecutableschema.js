b275affbb7c3e71b859ca128ae86e94dd9002ee0
Akash Manohar J <akash@akash.im>
2018-04-18 10:53:24 +0530

    Code with comments

--- stitcher/example-makeremoteexecutableschema.js
+++ stitcher/example-makeremoteexecutableschema.js
@@ -3,17 +3,34 @@ const { HttpLink } = require('apollo-link-http');
 const fetch = require('node-fetch');
 
 const url = 'http://graphql-tryout-rails.herokuapp.com/graphql';
-const link = new HttpLink({ uri: url, fetch });
 
+// USING HttpLink
+// const link = new HttpLink({ uri: url, fetch });
+
+// Using apollo-fetch
 // const { createApolloFetch } = require('apollo-fetch');
 // const fetcher = createApolloFetch({uri: url});
 
+
+// Using node-fetch
+const fetcher = async ({ query, variables, operationName, context }) => {
+  const fetchResult = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query, variables, operationName })
+  });
+  return fetchResult.json();
+};
+
+
 (async function () {
-  var schema = await introspectSchema(link);
+  var schema = await introspectSchema(fetcher);
 
   return makeRemoteExecutableSchema({
     schema: schema,
-    link,
+    fetcher,
   });
 
   console.log("Remote schema fetched");
